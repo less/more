de4c4f6a543763ec95ec0502092a1733150214c9
Dave Rapin <daverapin@dave-rapins-macbook-pro.local>
2010-01-18 00:49:31 +0800

    Modified the generate method to cache generated stylesheets in tmp/less-cache and check modified times in development mode. Greatly speeds up requests during development.

--- install.rb
+++ install.rb
@@ -2,3 +2,4 @@ require "fileutils"
 include FileUtils::Verbose
 
 mkdir_p File.join(Rails.root, "app", "stylesheets")
+mkdir_p File.join(Rails.root, "tmp", "less-cache")
--- lib/more.rb
+++ lib/more.rb
@@ -108,6 +108,10 @@ class Less::More
       pathname && pathname.exist?
     end
     
+    def cache_path
+      File.join(RAILS_ROOT, 'tmp', 'less-cache')
+    end
+            
     # Generates the .css from a .less or .lss file in Less::More.source_path matching
     # the given parameters.
     #
@@ -118,15 +122,31 @@ class Less::More
     def generate(path_as_array)
       source = pathname_from_array(path_as_array)
       
-      if source.extname == ".css"
-        css = File.read(source)
+      # check the tmp directory for an existing file
+      destination_dir = source.dirname.to_s.gsub(source_path, cache_path)
+      destination = File.join(destination_dir, source.basename.to_s.gsub('.less', '.css').gsub('.lss', '.css'))
+      
+      # compare the modified times
+      if File.exists?(destination) and File.new(destination).mtime >= File.new(source).mtime
+        # cached destination file is the same as the source, just return the cached file
+        css = File.read(destination)
       else
-        engine = File.open(source) {|f| Less::Engine.new(f) }
-        css = engine.to_css
-        css.delete!("\n") if self.compression?
-        css = (HEADER % [source.to_s]) << css if self.header?
+        if source.extname == ".css"
+          css = File.read(source)
+        else
+          engine = File.open(source) {|f| Less::Engine.new(f) }
+          css = engine.to_css
+          css.delete!("\n") if self.compression?
+          css = (HEADER % [source.to_s]) << css if self.header?
+        end
+        # write the file to our cache
+        FileUtils.mkdir_p destination_dir
+        File.open(destination, "w") {|f|
+          f.puts css
+        }
       end
 
+      # return the css
       css
     end
     
